35a54bc8e2726f09381d3bfda6ec371eda451e7f
dannikaml <dannikaml@hotmail.com>
2022-12-18 19:35:33 -0600

    add functions and var

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,15 @@
 // Assignment code here
 
+var characterLength = 8-128;
+var randomArr = [];
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -13,23 +20,32 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 
-
-function getRandomNumber(); {
+function generatePassword(); {
 
 }
 
+function getCriteria(); {
+
+}
+
+function getRandomNumber(); {
+  var Numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  
+}
+
 function getRandomUpper(); {
+  var Upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 }
 
 function getRandomLower(); {
+  var Lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 }
 
 function getRandomSymbol(); {
-  
+  var Symbol = [' #', '$', '%', '&', '(', ')', '!', '*', '+', '-', '<', '?', '@'];
+
  }
